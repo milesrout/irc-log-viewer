3a9d28f0e048d332845dfb57d7c1de0a873763d2
Miles Rout <miles.rout@gmail.com>
2017-11-05 16:31:42 +1300

    Make database configurable, and rename table that is used

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var sqlite3 = require('sqlite3').verbose();
 
 var app = express();
-var db = new sqlite3.Database('irc.db');
+var db = new sqlite3.Database(process.argv[2]);
 
 app.use(function(req, res, next) {
 	res.set('Access-Control-Allow-Origin', '*');
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/initial', function(req, res) {
-	db.all('SELECT * from main ORDER BY id DESC LIMIT 50', function(err, rows) {
+	db.all('SELECT * from messages ORDER BY id DESC LIMIT 50', function(err, rows) {
 		if (err) {
 			console.error(err);
 			res.sendStatus(500);
@@ -27,7 +27,7 @@ app.get('/more-after', function(req, res) {
 	}
 	const id = parseInt(req.query.id, 10);
 	console.log('after', {id});
-	db.all('SELECT * from main WHERE id > ? ORDER BY id ASC LIMIT 50', id, function(err, rows) {
+	db.all('SELECT * from messages WHERE id > ? ORDER BY id ASC LIMIT 50', id, function(err, rows) {
 		if (err) {
 			console.error(err);
 			return res.sendStatus(500);
@@ -43,7 +43,7 @@ app.get('/more-before', function(req, res) {
 
 	const id = parseInt(req.query.id, 10);
 	console.log('before', {id});
-	db.all('SELECT * from main WHERE id < ? ORDER BY id DESC LIMIT 50', id, function(err, rows) {
+	db.all('SELECT * from messages WHERE id < ? ORDER BY id DESC LIMIT 50', id, function(err, rows) {
 		if (err) {
 			console.error(err);
 			res.sendStatus(500);
